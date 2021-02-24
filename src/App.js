49606b79479714ec998030e9beea56cfd00a1677
Matina <matinasaura@gmail.com>
2021-02-24 05:39:06 -0500

    Auth0 setup part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from './assets/skate.png';
 import './App.css';
 import React from "react";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to (idk what to name it ^_^)!
-        </p>
-      </header>
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+    </>
+    
   );
 }
 
 export default App;
+/// <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Welcome to (idk what to name it ^_^)!
+    //     </p>
+    //   </header>
+    // </div>
+    
