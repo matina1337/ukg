5e08f3b53269ab0e4a4201829d11e1d35ccd008c
Matina <matinasaura@gmail.com>
2021-03-03 09:12:01 -0500

    starting to put together dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -4,13 +4,12 @@ import React from "react";
 function Dashboard() {
     return (
     	<div id = "Dash">
-    	<div id = "MovesList"> </div>
-    	<div>
-	    	<button className = "Create"> + Create New Program </button>
-	    	<button className = "Create"> Find Saved Program </button>
-    	</div>
-    	
-    	
+    		<div id = "MovesList"> </div>
+    		<div id = "Panel">
+    			<div id = "Grid"> </div>
+    			<div id = "Judges"> Judging Panel </div>
+    			<div id = "Judges"> Upload Music </div>
+    		</div>
     	</div>
     );
 }
