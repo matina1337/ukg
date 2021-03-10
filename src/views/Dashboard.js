5f2ed200ec76153dc8d085f52a5529a53a600934
Matina <matinasaura@gmail.com>
2021-03-10 00:14:31 -0500

    mostly done with interface

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,31 +1,83 @@
 import './Dashboard.css';
-import React from "react";
+import React, { useState } from "react";
 import Nutcracker from "./test.mp3";
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import Button from 'react-bootstrap/Button';
 
 
-function Dashboard() {
-let audio = new Audio("./test.mp3")
+export default function Dashboard() {
+
+let audio = new Audio("./test.mp3") //playing audio
 const start = () => {
     audio.play()
+
 }
+
+const [isOpen, setIsOpen] = useState(false); // modal
+function toggleModal() {
+    setIsOpen(!isOpen);
+ }
+
+
+
     return (
     	<div id = "Dash">
     		<div id = "Moves"> 
 	    		<div id = "MovesList">
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
-	    			<div id = "Move"> New Move </div>
+	    			<div id = "MoveCont">
+		    			<div id = "Move"> Move 1 </div>
+
+		    			<button onClick = {toggleModal}> + </button>
+		    			      <Modal
+						        isOpen={isOpen}
+						        onRequestClose={toggleModal}
+						        contentLabel="My dialog"
+						        className="mymodal"
+						        overlayClassName="myoverlay"
+						        closeTimeoutMS={500}
+						      >
+						      	<div id = "Modal_head">
+							      	<div id = "Modal_title">Move 1</div>
+							        <button id = "Close_modal" onClick={toggleModal}>x</button>
+							    </div>
+
+								<select name="type" class="Select">
+									<option value="default">Type</option>
+								  <option value="jump">Jump</option>
+								  <option value="spin">Spin</option>
+								  <option value="choreo">Choreography</option>
+								</select>
+
+								<div id = "jump">
+
+									<select name="jump" class="Select">
+										<option value="default">Jump</option>
+									  <option value="sal">Salchow</option>
+									  <option value="toe">Toe Loop</option>
+									  <option value="loop">Loop</option>
+									  <option value="flip">Flip</option>
+									  <option value="lutz">Lutz</option>
+									  <option value="axel">Axel</option>
+									</select>
+
+									<select name="rotations" class="Select">
+										<option value="default">#</option>
+									  <option value="sal">1</option>
+									  <option value="toe">2</option>
+									  <option value="loop">3</option>
+									  <option value="flip">4</option>
+									</select>
+
+								</div>
+
+								<input type="text" className = "Time" placeholder = "Minutes"></input>
+								<input type="text" className = "Time" placeholder = "Seconds"></input>
+
+
+						      </Modal>
+	    			</div>
+
 	    		</div>
 
     			<button id = "AddMove"> + Add Move </button>
@@ -46,6 +98,4 @@ const start = () => {
     	</div>
     );
 }
-
-export default Dashboard;
     
